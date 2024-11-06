26f562f3cd9414a8e25063365135638c9f0b9749
Abraham Rodriguez <arm.developer00@gmail.com>
2024-11-06 16:15:22 +0100

    getters and setters y constructores de los dto

--- src/main/java/com/es/dto/PeliculaDTO.java
+++ src/main/java/com/es/dto/PeliculaDTO.java
@@ -10,4 +10,93 @@ public class PeliculaDTO {
     private String screenshot;
     private String synopsis;
     private Double rating;
+
+    //Constructor vacio
+    public PeliculaDTO() {}
+
+    //Constructor con parametros
+    public PeliculaDTO(String title, String director, String time, String trailer, String posterImage, String screenshot, String synopsis, Double rating) {
+        this.title = title;
+        this.director = director;
+        this.time = time;
+        this.trailer = trailer;
+        this.posterImage = posterImage;
+        this.screenshot = screenshot;
+        this.synopsis = synopsis;
+        this.rating = rating;
+    }
+
+    //Getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getTrailer() {
+        return trailer;
+    }
+
+    public void setTrailer(String trailer) {
+        this.trailer = trailer;
+    }
+
+    public String getPosterImage() {
+        return posterImage;
+    }
+
+    public void setPosterImage(String posterImage) {
+        this.posterImage = posterImage;
+    }
+
+    public String getScreenshot() {
+        return screenshot;
+    }
+
+    public void setScreenshot(String screenshot) {
+        this.screenshot = screenshot;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
 }
--- src/main/java/com/es/dto/SesionDTO.java
+++ src/main/java/com/es/dto/SesionDTO.java
@@ -7,4 +7,44 @@ public class SesionDTO {
     private Long movieId;
     private Long roomId;
     private LocalDate date;
+
+    //Constructor con parámetros
+    public SesionDTO(Long movieId, Long roomId, LocalDate date) {
+        this.movieId = movieId;
+        this.roomId = roomId;
+        this.date = date;
+    }
+
+    //Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Long getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(Long roomId) {
+        this.roomId = roomId;
+    }
+
+    public Long getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(Long movieId) {
+        this.movieId = movieId;
+    }
 }
