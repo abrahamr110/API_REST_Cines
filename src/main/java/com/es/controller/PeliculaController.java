f13cae8c27fe1f58a0595bdfe03ef9d505fe7619
Abraham Rodriguez <arm.developer00@gmail.com>
2024-11-06 16:34:54 +0100

    métodos get Pelicula finalizados

--- src/main/java/com/es/controller/PeliculaController.java
+++ src/main/java/com/es/controller/PeliculaController.java
@@ -3,26 +3,34 @@ package com.es.controller;
 import com.es.dto.PeliculaDTO;
 import com.es.service.PeliculaService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/peliculas")
 public class PeliculaController {
-
     private final PeliculaService peliculaService;
 
     public PeliculaController(PeliculaService peliculaService) {
         this.peliculaService = peliculaService;
     }
+    @GetMapping("/")
+    public ResponseEntity<PeliculaDTO> getAll() {
+        return ResponseEntity.ok(peliculaService.getAll());
+    }
 
-    @PostMapping
+    @GetMapping("/{id}")
+    public ResponseEntity<PeliculaDTO> getPelicula(@PathVariable Long id) {
+        return ResponseEntity.ok(peliculaService.getPelicula(id));
+    }
+
+    @GetMapping("/rating/{minRating}")
+    public ResponseEntity<PeliculaDTO> getByMinRating(@PathVariable Double minRating) {
+        return ResponseEntity.ok(peliculaService.getByMinRating(minRating));
+    }
+
+    @PostMapping("/")
     public ResponseEntity<PeliculaDTO> createPelicula(
             @RequestBody PeliculaDTO peliculaDTO) {
         return ResponseEntity.ok(peliculaService.createPelicula(peliculaDTO));
     }
-
-   
 }
--- src/main/java/com/es/service/PeliculaService.java
+++ src/main/java/com/es/service/PeliculaService.java
@@ -3,14 +3,34 @@ package com.es.service;
 import com.es.dto.PeliculaDTO;
 import com.es.entities.Pelicula;
 import com.es.repository.PeliculaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PeliculaService {
+    @Autowired
     private final PeliculaRepository peliculaRepository;
 
     public PeliculaService(PeliculaRepository peliculaRepository) {
         this.peliculaRepository = peliculaRepository;
+    }
+    public PeliculaDTO getAll(){
+        Pelicula pelicula = peliculaRepository.findAll().get(0);
+        return mapToDTO(pelicula);
+    }
+
+    public PeliculaDTO getPelicula(Long id) {
+        Pelicula pelicula = peliculaRepository.findById(id).orElse(null);
+        return mapToDTO(pelicula);
+    }
+
+    public PeliculaDTO getByMinRating(Double minRating){
+        for(Pelicula pelicula : peliculaRepository.findAll()){
+            if(pelicula.getRating() > minRating){
+                return mapToDTO(pelicula);
+            }
+        }
+        return null;
     }
 
     public PeliculaDTO createPelicula(PeliculaDTO peliculaDTO) {
